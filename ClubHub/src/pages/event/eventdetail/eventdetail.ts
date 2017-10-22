635cbf37bf782feabe758e4c23ec428c338199b5
Bwatermelon <bdsb2011@gmail.com>
2017-10-22 14:04:14 +1000

    slides debug

--- ClubHub/src/pages/event/eventdetail/eventdetail.ts
+++ ClubHub/src/pages/event/eventdetail/eventdetail.ts
@@ -17,7 +17,23 @@ import { NavController, NavParams,ViewController} from 'ionic-angular';
 export class EventDetailPage {
 
   event:any;
-
+slides = [
+    {
+      title: "Welcome to the Docs!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "img/buffet.jpg",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "img/2.jpg",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "img/dance.jpg",
+    }
+  ];
 
 
   constructor(public viewCtrl:ViewController,public navCtrl: NavController,public navParams:NavParams) {
